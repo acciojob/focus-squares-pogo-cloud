e44dc105a227ac1788417ee3f3d97ce350a50f82
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-15 23:43:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ let squares=document.querySelectorAll(".square");
 for(let t of squares){
 	t.addEventListener("mouseover",(event)=>{
 		for(let s of squares){
-            s.style.backgroundColor = "#E6E6FA";
+            // s.style.backgroundColor = "#E6E6FA";
+			if(s !== t) {
+               s.style.backgroundColor = "#6F4E37"; // Coffee
+           }
+			
         }
-		console.log(t.id);
+		// console.log(t.id);
 		
-		if(t.id=="square1"){
-			document.querySelector("#square2").style.backgroundColor="#6F4E37";
-			document.querySelector("#square3").style.backgroundColor="#6F4E37";
+		// if(t.id=="square1"){
+		// 	document.querySelector("#square2").style.backgroundColor="#6F4E37";
+		// 	document.querySelector("#square3").style.backgroundColor="#6F4E37";
 			
-		}else if(t.id=="square2"){
-			document.querySelector("#square1").style.backgroundColor="#6F4E37";
-		    document.querySelector("#square3").style.backgroundColor="#6F4E37";
+		// }else if(t.id=="square2"){
+		// 	document.querySelector("#square1").style.backgroundColor="#6F4E37";
+		//     document.querySelector("#square3").style.backgroundColor="#6F4E37";
 			
-		}else if(t.id=="square3"){
-			document.querySelector("#square1").style.backgroundColor="#6F4E37";
-			document.querySelector("#square2").style.backgroundColor="#6F4E37";
-		}
+		// }else if(t.id=="square3"){
+		// 	document.querySelector("#square1").style.backgroundColor="#6F4E37";
+		// 	document.querySelector("#square2").style.backgroundColor="#6F4E37";
+		// }
 	})
 	t.addEventListener("mouseleave",(event)=>{
 		for(let s of squares){

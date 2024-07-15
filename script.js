8a478202ce10e7dd9648b52263098c77b1c9ca9b
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-07-15 18:43:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+let squares=document.querySelectorAll(".square");
+for(let t of squares){
+	t.addEventListener("click",(event)=>{
+		for(let s of squares){
+            s.style.backgroundColor = "#E6E6FA";
+        }
+		if(t.id=="square1"){
+			document.querySelector("#square2").style.backgroundColor="#6F4E37";
+			document.querySelector("#square3").style.backgroundColor="#6F4E37";
+			
+		}else if(t.id=="square2"){
+			document.querySelector("#square1").style.backgroundColor="#6F4E37";
+		    document.querySelector("#square3").style.backgroundColor="#6F4E37";
+			
+		}else if(t.id=="square3"){
+			document.querySelector("#square1").style.backgroundColor="#6F4E37";
+			document.querySelector("#square2").style.backgroundColor="#6F4E37";
+		}
+	})
+}
